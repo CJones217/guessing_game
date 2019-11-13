bb28e48c3dc2cd256f77cdcf3569414486a74f49
Collin Jones <cjones28@luc.edu>
2019-11-12 23:14:36 -0600

    implemented loop

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,26 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
+    loop {
+        println!("input your guess.");
 
-    println!("input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse() // defining guess again is an example of shadowing trim removes white space and parse parses string into number (need to say the type of number)
+            .expect("you need to type a number");
 
-    let guess: u32 = guess.trim().parse() // defining guess again is an example of shadowing trim removes white space and parse parses string into number (need to say the type of number)
-        .expect("you need to type a number");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Correct"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Correct! goodbye");
+                break;
+            },
+        }
     }
 }
