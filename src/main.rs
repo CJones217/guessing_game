154f0ddc0bf732682e508048601ecd460761dab5
Collin Jones <cjones28@luc.edu>
2019-11-12 23:07:07 -0600

    implement match cases

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -8,12 +9,21 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    println!("input your guess.");
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse() // defining guess again is an example of shadowing trim removes white space and parse parses string into number (need to say the type of number)
+        .expect("you need to type a number");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("Correct"),
+    }
 }
